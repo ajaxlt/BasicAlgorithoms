ac5549c4bc427069f384294c3c1d560e9169df19
ajaxlt <510586047@qq.com>
2019-05-16 18:29:45 +0800

    -

--- 字符串/KMP.cpp
+++ 字符串/KMP.cpp
@@ -1,40 +1,60 @@
 #include <iostream>
-#include <vector>
 #include <string>
-/*
- * KMP算法:
- * next[i] 表示模式串的 0 ~ i 中 前缀后缀相同的最大长度
- *
- * 我们用 i 指针指向主串, j 指针指向模式串。如果我们发现当前位置不匹配，使用暴力法会同时回溯 i, j 两指针
- * 但是，更加高效的做法应该是保持 i 指针不动, 移动 j 指针
- * 因为，我们匹配的时候，虽然有时遭遇失败，但不代表这次匹配全是无用的
- * 起码，我们验证了模式串 j 之前的子串能够与主串匹配。
- * 我们移动 j， 使得与后缀相同的前缀移动到后缀的位置，则这一部分仍然是匹配的，可以继续往后尝试，直到 j 移动到左端点
- * 用 next[j] 去表示每次匹配失败时 j 应该移到的位置
- */
+#include <vector>
 using namespace std;
-void getNext(string p, vector<int>& next) {
-    next[0] = -1;
-    int i = 0, j = -1;
-    while(i < p.size()) {
-        if (j == -1 || p[i] == p[j]) next[++i] = ++j;
-        else j = next[j];
+
+/**
+ * @param p 模式串
+ * @return 模式串的部分匹配表(往后挪一位)
+ * p[i] 表示
+ * 以 i 结尾的最长相同前后缀中, 前缀的终止字符下标, -1 表示没有相同的前后缀
+ * p[i] + 1 表示
+ * 以 i 结尾的最长相同前后缀的长度
+ *
+ * 动态规划求解
+ */
+vector<int> getNext(const string& p) {
+    int len = p.size();
+    vector<int> next(len, -1);
+    for (int i = 1; i < len; ++i) {
+        int j = next[i - 1];
+        // 令临时量 j 初始化为 next[i - 1],
+        // 那么 j 的另一层含义为 以 i - 1 结尾的前缀的终止字符下标
+        // 然后可以用动态规划的思路求解
+        while(p[j + 1] != p[i] && j >= 0) {
+            // 如果 i - 1 结尾的前缀的后一个p[j + 1]与扩充的这一位 p[i] 不等
+            // 则要向前回溯变量 j, 直到可以相等为止，这个时候， j 是越来越小的
+            j = next[j];
+        }
+        if (p[j + 1] == p[i]) next[i] = j + 1;
+        //else next[i] = -1;
     }
+    return next;
 }
-int KMP(string t, string p) {
+
+/**
+ * @param s 主串串
+ * @param p 模式串
+ * return 第一次匹配时, 主串的起始匹配位置，如果不匹配，则返回 -1
+ *
+ */
+int KMP (const string& s, const string& p) {
+    int len1 = s.size(), len2 = p.size();
+    vector<int> next = getNext(p);
     int i = 0, j = 0;
-    vector<int> next(p.size());
-    getNext(p, next);
-    int len1 = t.size(), len2 = p.size();
     while(i < len1 && j < len2) {
-        if (j == -1 || t[i] == p[j]) ++i, ++j;
-        else j = next[j];
+        if (s[i] == p[j]) {
+            ++i;
+            ++j;
+        }
+        else if (j == 0) ++i;
+        // 当 j = 0 时，如果再回溯 j = -1
+        // 所以，j = 0 其实已经表示本次以 i 开头的匹配宣告失败, 从下一位 i + 1 继续开始
+
+        else j = next[j - 1] + 1;
+        // 当 j != 0 时，说明回退 j 还有可能匹配上
+        // 即 让模式串 向后移动 next[j - 1] + 1 位
     }
     if (j == len2) return i - j;
     else return -1;
-}
-int main() {
-    string t = "abababca", p = "bca";
-    cout << KMP(t, p) << endl;
-    return 0;
 }
